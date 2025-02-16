8977082dff9aa7d5e8a586c9e2ee51c39baec0e8
Allan Lewis <allan.e.lewis@gmail.com>
2025-02-16 09:46:44 -0500

    TRaefik

--- argocd/bootstrap-argo-cd.sh
+++ argocd/bootstrap-argo-cd.sh
@@ -16,4 +16,4 @@ kubectl apply -n argocd -f $SCRIPT_DIR/kustomization.yaml
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
-kubectl port-forward svc/argocd-server -n argocd 8181:443
+kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8181:443
