08b74bf46032696526cb0d841c4080f9b467fdf4
allan-lewis <allan.e.lewis@gmail.com>
2025-03-09 18:26:21 -0400

    Terraform

--- terraform/environments/staging-k8s/variables.tf
+++ terraform/environments/staging-k8s/variables.tf
@@ -40,17 +40,17 @@ variable "worker_list" {
       gateway           = "192.168.86.1"
       tags              = ["terraform", "staging", "talos", "worker"]
     }
-    # delta = {
-    #   description       = "Talos Linux worker node /w OpenVPN gateway (managed by Terraform)"
-    #   proxmox_node      = "polaris"
-    #   cpu_cores         = 2
-    #   memory_dedicated  = 4096
-    #   datastore_id      = "local-lvm"
-    #   disk_size         = 64
-    #   ipv4_address      = "192.168.86.93"
-    #   gateway           = "192.168.86.123"
-    #   tags              = ["terraform", "staging", "talos", "worker"]
-    # }
+    delta = {
+      description       = "Talos Linux worker node /w OpenVPN gateway (managed by Terraform)"
+      proxmox_node      = "polaris"
+      cpu_cores         = 2
+      memory_dedicated  = 4096
+      datastore_id      = "local-lvm"
+      disk_size         = 64
+      ipv4_address      = "192.168.86.93"
+      gateway           = "192.168.86.123"
+      tags              = ["terraform", "staging", "talos", "worker"]
+    }
   }
 }
 
