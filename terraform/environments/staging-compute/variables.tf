9ee0b7923cd99b5a2c52dc57c664269d6e9d0a93
allan-lewis <allan.e.lewis@gmail.com>
2025-03-28 19:06:31 -0400

    Media

--- terraform/environments/staging-compute/variables.tf
+++ terraform/environments/staging-compute/variables.tf
@@ -12,6 +12,17 @@ variable "vm_list_ubuntu" {
       gateway           = "192.168.86.1"
       tags              = ["terraform", "staging", "ubuntu"]
     }
+    regulus = {
+      description       = "Media host (managed by Terraform)"
+      proxmox_node      = "polaris"
+      cpu_cores         = 4
+      memory_dedicated  = 8192
+      datastore_id      = "local-lvm"
+      disk_size         = 64
+      ipv4_address      = "192.168.86.116/24"
+      gateway           = "192.168.86.1"
+      tags              = ["terraform", "production", "ubuntu", "media"]
+    }
   }
 }
 
