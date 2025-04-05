8d43655b05b79034bd547bb7531b6198f3879a3e
allan-lewis <allan.e.lewis@gmail.com>
2025-04-05 14:12:01 -0400

    Terraform

--- terraform/environments/staging-compute/variables.tf
+++ terraform/environments/staging-compute/variables.tf
@@ -12,18 +12,6 @@ variable "vm_list_ubuntu" {
       gateway           = "192.168.86.1"
       tags              = ["terraform", "staging", "ubuntu"]
     }
-    regulus = {
-      description       = "Media host (managed by Terraform)"
-      proxmox_node      = "polaris"
-      cpu_cores         = 4
-      memory_dedicated  = 8192
-      datastore_id      = "local-lvm"
-      disk_size         = 64
-      ipv4_address      = "192.168.86.116/24"
-      gateway           = "192.168.86.1"
-      tags              = ["terraform", "production", "ubuntu", "media"]
-    }
-  }
 }
 
 # Set this from an environment variable called TF_VAR_PROXMOX_VM_PUBLIC_KEY
