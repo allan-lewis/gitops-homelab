90fd64fe7f4fcb0ac458ff72da9fa5dbf3c80ce0
allan-lewis <allan.e.lewis@gmail.com>
2025-04-29 19:17:43 -0400

    Terraform

--- terraform/environments/production-k8s/variables.tf
+++ terraform/environments/production-k8s/variables.tf
@@ -22,9 +22,9 @@ variable "worker_list" {
       description       = "Talos Linux worker node (managed by Terraform)"
       proxmox_node      = "sirius"
       cpu_cores         = 4
-      memory_dedicated  = 8192
+      memory_dedicated  = 12288
       datastore_id      = "ssd0"
-      disk_size         = 512
+      disk_size         = 640
       ipv4_address      = "192.168.86.151"
       gateway           = "192.168.86.1"
       tags              = ["terraform", "production", "talos", "worker"]
