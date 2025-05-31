5a0dcf603ef7af5a3fff49fb72eab9467c4106ed
allan-lewis <allan.e.lewis@gmail.com>
2025-05-30 20:11:41 -0400

    DevOps Staging

--- terraform/environments/devops/backend.tf
+++ terraform/environments/devops/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "allans-homelab-terraform-state-bucket"
+    bucket         = "gitops-homelab-terraform"
     key            = "devops/state.tfstate"   # Path inside the bucket
     region         = "us-east-1"
     encrypt        = true  # Ensures state is encrypted
