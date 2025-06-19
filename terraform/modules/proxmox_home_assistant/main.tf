798186920346b347ba3f2e01eec879ecd67bdd39
allan-lewis <allan.e.lewis@gmail.com>
2025-06-18 22:21:21 -0400

    #180 - Home Assistant

--- terraform/modules/proxmox_home_assistant/main.tf
+++ terraform/modules/proxmox_home_assistant/main.tf
@@ -10,19 +10,31 @@ terraform {
 resource "proxmox_virtual_environment_vm" "vm_haos" {
   for_each = var.vm_list
 
-  name        = each.key
-  node_name   = each.value.proxmox_node
+  name      = each.key
+  node_name = each.value.proxmox_node
   description = each.value.description
-  tags        = each.value.tags
-  on_boot     = true
 
   cpu {
     cores = each.value.cpu_cores
-    type  = "x86-64-v2-AES"
+    type = "x86-64-v2-AES"
   }
 
   memory {
     dedicated = each.value.memory_dedicated
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = each.value.ipv4_address
+        gateway = each.value.gateway
+      }
+    }
+
+    user_account {
+      username = "lab"
+      keys = var.public_keys
+    }
   }
 
   disk {
@@ -31,18 +43,13 @@ resource "proxmox_virtual_environment_vm" "vm_haos" {
     interface    = "scsi0"
     iothread     = true
     discard      = "on"
+    size         = each.value.disk_size
   }
 
   network_device {
     bridge = "vmbr0"
   }
 
-  display {
-    type = "serial0"
-  }
-
-  serial_device {
-    id   = "serial0"
-    type = "socket"
-  }
+  tags = each.value.tags
+  on_boot     = true
 }
