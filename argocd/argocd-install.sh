be00de5e664db8586ebb085f49ee914ac4ba69f1
allan-lewis <allan.e.lewis@gmail.com>
2025-06-28 17:54:34 -0400

    #178 - ArgoCD Helm

--- argocd/argocd-install.sh
+++ argocd/argocd-install.sh
@@ -2,12 +2,21 @@
 
 set -eu
 
+if [ -z "$DOPPLER_TOKEN" ]; then
+  echo "DOPPLER_TOKEN is not set; exiting"
+  exit 1
+fi
+
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl create secret generic doppler-token --from-literal dopplerToken=$DOPPLER_TOKEN --dry-run=client -o yaml | kubectl apply -f -
+
 doppler run --command "envsubst < argocd-helm-values-template.yaml > /tmp/argocd-values.yaml"
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-helm install argocd argo/argo-cd -n argocd --create-namespace -f /tmp/argocd-values.yaml
+helm install argocd argo/argo-cd -n argocd --create-namespace --version 7.9.1 -f /tmp/argocd-values.yaml
 
 # helm upgrade argocd argo/argo-cd --namespace argocd --version 7.9.1 -f /tmp/argocd-values.yaml
 # kubectl rollout restart deployment argocd-server -n argocd
