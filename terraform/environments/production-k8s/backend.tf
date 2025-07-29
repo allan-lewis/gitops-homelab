a321efa1b3825fede0fa3ce43027e6c0962d4318
allan-lewis <allan.e.lewis@gmail.com>
2025-07-29 18:02:31 -0400

    #110 - Terraform S3 bucket

--- terraform/environments/production-k8s/backend.tf
+++ terraform/environments/production-k8s/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "allans-homelab-terraform-state-bucket"
+    bucket         = "gitops-homelab-terraform"
     key            = "gitops-homelab-production-k8s/state.tfstate"   # Path inside the bucket
     region         = "us-east-1"
     encrypt        = true  # Ensures state is encrypted
