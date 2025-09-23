d00d82a3756b336d82bf82ffdd3172a9d90479e5
Ubuntu <allan.e.lewis@gmail.com>
2025-09-23 14:18:38 -0400

    #178 - ArgoCD Helm

--- argocd/argocd-install.sh
+++ argocd/argocd-install.sh
@@ -14,10 +14,10 @@ doppler run --command "envsubst < argocd-helm-values-template.yaml > /tmp/argocd
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-helm install argocd argo/argo-cd -n argocd --create-namespace --version 7.9.1 -f /tmp/argocd-values.yaml
+helm install argocd argo/argo-cd -n argocd --create-namespace --version 8.5.6 -f /tmp/argocd-values.yaml
 
 kubectl apply -f $CONFIG_MAP
 
-# helm upgrade argocd argo/argo-cd --namespace argocd --version 7.9.1 -f /tmp/argocd-values.yaml
+# helm upgrade argocd argo/argo-cd --namespace argocd --version 8.5.6 -f /tmp/argocd-values.yaml
 # kubectl rollout restart deployment argocd-server -n argocd
 # kubectl rollout restart deployment argocd-dex-server -n argocd
