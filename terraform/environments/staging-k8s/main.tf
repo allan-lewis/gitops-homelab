3a978869427bdd23aa216f955ee54224d2675389
Allan Lewis <allan.e.lewis@gmail.com>
2025-10-27 12:43:08 -0400

    #354: reorganize inventory

--- terraform/environments/staging-k8s/main.tf
+++ terraform/environments/staging-k8s/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.84.0"
+    }
+  }
+}
+
 module "staging_proxmox_talos_cluster" {
   source = "../../modules/proxmox_talos_cluster"
   
@@ -8,6 +17,14 @@ module "staging_proxmox_talos_cluster" {
   cluster_name = "k8s-staging"
 
   node_name = "polaris"
+}
+
+# default config to satisfy anything that expects an unaliased provider
+provider "proxmox" {
+  endpoint = var.PROXMOX_VE_ENDPOINT_POLARIS
+  username = "root@pam"
+  password = var.PROXMOX_VE_PASSWORD_POLARIS
+  insecure = false   # or true if you must skip TLS validation
 }
 
 output "talosconfig" {
