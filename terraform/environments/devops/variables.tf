9a54cc81b3cac9a6f31dd135db851e66feb184dc
Allan Lewis <allan.e.lewis@gmail.com>
2025-11-29 22:37:33 -0500

    405: decommission callahan

--- terraform/environments/devops/variables.tf
+++ terraform/environments/devops/variables.tf
@@ -1,17 +1,6 @@
 variable "vm_list_ubuntu" {
   description = "A list of Ubuntu VMs to create"
   default = {
-    callahan = {
-      description       = "devops host for the staging environment (managed by terraform)"
-      proxmox_node      = "polaris"
-      cpu_cores         = 2
-      memory_dedicated  = 4096
-      datastore_id      = "local-lvm"
-      disk_size         = 64
-      ipv4_address      = "192.168.86.215/24"
-      gateway           = "192.168.86.1"
-      tags              = ["terraform", "devops", "ubuntu"]
-    }
     barlow = {
       description       = "devops host for the production environment (managed by terraform)"
       proxmox_node      = "polaris"
