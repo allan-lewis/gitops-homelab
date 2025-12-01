194ab43d626404a6a099598d0d528d5348d5c340
Allan Lewis <allan.e.lewis@gmail.com>
2025-12-01 16:34:07 -0500

    406: decommission barlow

--- terraform/environments/devops/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gitops-homelab-terraform"
-    key            = "devops/state.tfstate"   # Path inside the bucket
-    region         = "us-east-1"
-    encrypt        = true  # Ensures state is encrypted
-  }
-}
